ea18b468690d6f0e9a0cc5de909c9d5e72b699a0
MAInformatico <magutierrezsistemas@gmail.com>
2017-05-14 11:49:26 +0200

    another one

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 rm script.txt;
 sudo apt-get update -y > script.txt; sudo apt-get upgrade -y > script.txt;
-sudo mpack -s "Actualizaciones" /root/script.txt magutierrezsistemas@gmail.com;
+sudo mpack -s "Actualizaciones" /root/script.txt yourmail;
 bleachbit --clean apt.*
 
