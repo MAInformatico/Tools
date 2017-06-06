b11fde092bb682747915f3da49a527a8cae2e8e4
Miguel Angel Gutiérrez <magutierrezsistemas@gmail.com>
2017-06-06 19:27:12 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,8 @@
+# Execute it as root
 rm script.txt;
-sudo apt-get update -y > script.txt; sudo apt-get upgrade -y > script.txt; #storing execution results
-sudo mpack -s "Updates" /root/script.txt yourmail; #sending it by email
+apt-get update -y > script.txt; apt-get upgrade -y > script.txt; #storing execution results
 bleachbit --clean apt.*
 #filtering a log file
 sed -i ".bak" 'beginning_filter_sentences' /var/directory/file.log;
-rm /var/directory/file.bak #deleting bak file (if you want)
+rm /var/directory/file.bak; #deleting bak file (if you want)
+mpack -s "Updates" /root/script.txt yourmail; #sending it by email
