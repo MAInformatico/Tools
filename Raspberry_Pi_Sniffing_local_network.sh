eeaf806d542105b509cf56bc5738e3483b2847f0
Miguel Angel Gutiérrez <magutierrezsistemas@gmail.com>
2019-02-12 23:00:11 +0100

    Update Raspberry_Pi_Sniffing_local_network.sh

--- Raspberry_Pi_Sniffing_local_network.sh
+++ Raspberry_Pi_Sniffing_local_network.sh
@@ -4,5 +4,5 @@
 #Use it under your own responsability!!!!!!!!!!!!
 
 
-sudo touch today$(date +%d%m%y).pcap && sudo tshark -nni <your device i.e.: eth0> -a duration:<time in seconds, i.e: 86400>  -w today$(date +%d%m%y).pcap && tar -cvf compress.tar hoy$(date +%d%m%y).pcap
+sudo touch today$(date +%d%m%y).pcap && sudo tshark -nni <your device i.e.: eth0> -a duration:<time in seconds, i.e: 86400>  -w today$(date +%d%m%y).pcap && tar -cvf compress.tar today$(date +%d%m%y).pcap
 echo "Sucess" | mail -s "<subject of the email>" <your-email>
