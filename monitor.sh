1e0251083123a38c2b17436e3c9369aa52508a92
Miguel Angel Gutiérrez <magutierrezsistemas@gmail.com>
2019-06-30 21:34:37 +0200

    Add files via upload
    Adding a Python script to check the temperature balance.

--- monitor.sh
+++ monitor.sh
@@ -1,12 +1,10 @@
-#Dependencies: nmap, mpack vcgencmd.
-#----------------------------------
-#sudo apt-get install nmap mpack; #Uncomment if you need to install
-sudo nmap -sP <local_broadcast>/24 > hosts.txt;
-sudo mpack -s "Current hosts" /root/hosts.txt your_email@email.com;
+sudo nmap -sP 192.168.1.0/24 > hosts.txt;
+sudo mpack -s "Current hosts" /root/hosts.txt magutierrezsistemas@gmail.com;
 sudo /opt/vc/bin/vcgencmd measure_temp > temperature.txt;
-sudo mpack -s "Server's temperature" /root/temperature.txt your_email@email.com;
-sudo ps -aux > processs.txt;
-sudo mpack -s "Current process" /root/process.txt your_email@email.com;
-sudo netstat -natp > ports.txt
-sudo mpack -s "Listening Ports" /root/ports.txt your_email@email.com;
-# rm *.txt #Be carefull if you execute this command
+python3 /root/temperature.py
+sudo mpack -s "Server's temperature" /root/temperature.txt magutierrezsistemas@gmail.com;
+#sudo ps -aux > process.txt;
+#sudo mpack -s "Current process" /root/process.txt magutierrezsistemas@gmail.com;
+#sudo netstat -natp > ports.txt;
+#sudo mpack -s "Listening Ports" /root/ports.txt magutierrezsistemas@gmail.com;
+rm hosts.txt;
---
+++ temperature.py
@@ -0,0 +1,26 @@
+import os, sys
+import subprocess
+
+#getting the current system temperature
+comand  = "/opt/vc/bin/vcgencmd measure_temp"
+currentResult = os.popen(comand).read()
+currentResult = currentResult[5:9]
+#reading the file
+with open('temperature.txt', 'r') as f:
+    lineas = [linea.split("=") for linea in f]
+f.close()
+#reading the file's temperature
+for linea in lineas:
+    temperatureReaded = linea[1]
+
+#parsing
+temperatureReaded = temperatureReaded[:4]
+
+f = open('temperature.txt', 'a')
+if currentResult > temperatureReaded:
+    f.write ("The temperature is increased\n" + currentResult)
+else:
+    f.write ("The temperatue is decreased\n" + temperatureReaded)
+
+f.close()
+
