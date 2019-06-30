5f9bca78d6134e6009a59ec8307f785adb54cd84
Miguel Angel Gutiérrez <magutierrezsistemas@gmail.com>
2019-06-30 21:42:55 +0200

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -3,8 +3,4 @@ sudo mpack -s "Current hosts" /root/hosts.txt magutierrezsistemas@gmail.com;
 sudo /opt/vc/bin/vcgencmd measure_temp > temperature.txt;
 python3 /root/temperature.py
 sudo mpack -s "Server's temperature" /root/temperature.txt magutierrezsistemas@gmail.com;
-#sudo ps -aux > process.txt;
-#sudo mpack -s "Current process" /root/process.txt magutierrezsistemas@gmail.com;
-#sudo netstat -natp > ports.txt;
-#sudo mpack -s "Listening Ports" /root/ports.txt magutierrezsistemas@gmail.com;
 rm hosts.txt;
