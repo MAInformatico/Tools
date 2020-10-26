d4218246cdedea88a3ad5d46b623eedcfb8e9c32
Miguel Angel Gutiérrez <magutierrezsistemas@gmail.com>
2020-10-26 21:01:58 +0100

    Add files via upload

--- initDockerJenkins.sh
+++ initDockerJenkins.sh
@@ -1,3 +1,4 @@
+docker pull jenkins/jenkins:lts;
 cd <path_for_the_containers_files>; docker run -p 8080:8080 -p 50000:50000 -v <path_for_the_containers_files>:/var/jenkins_home jenkins/jenkins:lts
 
 
