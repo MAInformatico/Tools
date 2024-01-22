e7d70f03a172d30eee7eb3313e9cf66a115a0db7
Miguel Angel Gutiérrez <magutierrezsistemas@gmail.com>
2024-01-22 18:23:19 +0100

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,6 +1,6 @@
 sudo nmap -sP 192.168.1.0/24 > hosts.txt;
-sudo mpack -s "Current hosts" /root/hosts.txt magutierrezsistemas@gmail.com;
+sudo mpack -s "Current hosts" /root/hosts.txt your@email.com;
 sudo /opt/vc/bin/vcgencmd measure_temp > temperature.txt;
 python3 /root/temperature.py
-sudo mpack -s "Server's temperature" /root/temperature.txt magutierrezsistemas@gmail.com;
+sudo mpack -s "Server's temperature" /root/temperature.txt your@email.com;
 rm hosts.txt;
