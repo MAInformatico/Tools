f16e123511fdc07bd4baa816f99c30deb2c4db38
Mike <magutierrezsistemas@gmail.com>
2024-01-22 19:54:00 +0100

    fixing issues related to tabs

--- avg.py
+++ avg.py
@@ -1,14 +1,14 @@
 def avg():
-    with open('history_temperature.txt') as lineas:
-	sum=0
+        lineas =  open('history_temperature.txt')
+        sum=0
         count=0
         for linea in lineas:
-		if linea == ' ':
-			pass
-		else: 
-		    sum+=float(linea)
-		count+=1
-    return str(round(sum/count,2))
+                if linea == ' ':
+                        pass
+                else:
+                    sum+=float(linea)
+                count+=1
+        return str(round(sum/count,2))
 
 def show():
     print('The average is: ' + avg() + '\n')
